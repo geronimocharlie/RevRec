63f4a0b6f3cad1c4cefdd751395b5dcd59c16a84
LJKS <Leon-Schmid@gmx.de>
2020-08-05 19:16:55 +0200

    efficiently creates batches now

--- integration_task.py
+++ integration_task.py
@@ -11,39 +11,30 @@ class Integration_Task():
      - loc: mean of the noise used as input
      - scale: scale of the noise used as input
     """
-    def __init__(self, discount=1., proto_length=100, loc=0, scale=1.):
+    def __init__(self, discount=1., length=100, size=1, batch_size=1, loc=0, scale=1.):
+        self.length = length
+        self.size = size
+        self.batch_size = batch_size
         self.discount = discount
-        self.proto_length = proto_length
         self.loc = loc
         self.scale = scale
 
-    def generate_sample(self, length=None, discount=None, loc=None, scale=None):
+    def generate_sample(self, length=None, size=None, batch_size=None, discount=None, loc=None, scale=None):
         """
         Function that samples an input target timesieres pair
         """
-        length = (length or self.proto_length)
+        length = (length or self.length)
+        size = (size or self.size)
+        batch_size = (batch_size or self.batch_size)
+        shape = (batch_size, length, size)
         discount = (discount or self.discount)
         loc = (loc or self.loc)
         scale = (scale or self.scale)
 
 
-        sample = np.expand_dims(np.random.normal(loc=loc, scale=scale, size=length), axis=-1)
-        target = discount_cumsum(sample, discount)
+        sample = np.random.normal(loc=loc, scale=scale, size=shape)
+        target = (discount_cumsum(sample, discount) > 0).astype(np.int)
         return sample, target
-
-    def generate_sample_batch(self, batch_size=1, length=None, discount=None, loc=None, scale=None):
-        """
-        @out:
-            samples: (batch_size, seq_lenght, input_size)
-            targets: (batch_size, seq_lenght, out_size)
-        """
-        samples = []
-        targets = []
-        for _ in range(batch_size):
-            s, t = self.generate_sample(length, discount, loc, scale)
-            samples.append(s)
-            targets.append(t)
-        return np.array(samples), np.array(targets)
 
 
 def discount_cumsum(x, discount):
@@ -59,4 +50,4 @@ def discount_cumsum(x, discount):
          x1 + discount * x2,
          x2]
     """
-    return signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+    return signal.lfilter([1], [1, float(-discount)], x[::-1], axis=1)[::-1]
